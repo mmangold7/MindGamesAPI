44ff95f041aeab36365a691e3dc0acdb453e1b28
Matt Mangold <mmangold7@gmail.com>
2022-11-26 19:08:35 -0500

    commiting this time

--- MindGamesApi/Startup.cs
+++ MindGamesApi/Startup.cs
@@ -31,10 +31,10 @@ public class Startup
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (env.IsDevelopment())
-        {
+        //if (env.IsDevelopment())
+        //{
             app.UseDeveloperExceptionPage();
-        }
+        //}
 
         app.UseHttpsRedirection();
 
